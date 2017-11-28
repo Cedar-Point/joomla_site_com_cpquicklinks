709d04f62c5177da659383d8d489e9f00f4213f2
dylan <dylan@belowaverage.org>
2017-11-28 16:50:50 -0500

    Final

--- cpquicklinks.php
+++ cpquicklinks.php
@@ -2,9 +2,23 @@
 defined('_JEXEC') or die;
 require(__DIR__.'./../../administrator/components/com_cpquicklinks/cpquicklinks_api.php');
 $links = getQuickLinks();
+if(!isset($_GET['option']) || $_GET['option'] !== 'com_cpquicklinks') {
+	$params = JFactory::getApplication()->getMenu()->getActive()->params;
+	$title = $params->get('pagetitle');
+	$css = $params->get('customcss');
+	if(!empty($css)) {
+		echo '<style>'.$css.'</style>';
+	}
+}
 ?>
 <link rel="stylesheet" href="components/com_cpquicklinks/src/css/index.css">
 <link rel="stylesheet" href="components/com_cpquicklinks/src/css/font-awesome.min.css">
+<?php
+if((!isset($_GET['option']) || $_GET['option'] !== 'com_cpquicklinks') && !empty($title)) {
+	$params = JFactory::getApplication()->getMenu()->getActive()->params;
+	echo '<h1 id="cpquicklinkstitle">'.$title.'</h1>';
+}
+?>
 <div id="cpquicklinks">
 <?php
 	foreach($links as $catname => $cat) {
@@ -30,7 +44,7 @@ $links = getQuickLinks();
 ?>
 </div>
 <?php 
-if(isset($_GET['task']) && $_GET['task'] == 'preview') {
+if(isset($_GET['option']) && $_GET['option'] == 'com_cpquicklinks') {
 	exit();
 }
 ?>
